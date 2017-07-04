cd14c399794bdcf92733c734a0699cdad40b8ab6
George Stricat <George.Stricat@endava.com>
2017-07-04 15:47:27 +0300

    Small changes 8

--- src/main/java/george/stricat/io/justgifit/controller/UploadController.java
+++ src/main/java/george/stricat/io/justgifit/controller/UploadController.java
@@ -38,7 +38,7 @@ import java.lang.invoke.MethodHandles;
             //TODO
         }
 
-        System.out.println("Small changes 7");
+        System.out.println("Small changes 8");
 
         LOG.info("Saved file to {}", videoFile.getAbsolutePath());
         return "";
