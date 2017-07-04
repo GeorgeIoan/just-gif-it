2da46fa463db6ef26e4c4dae99fbf6c7995fa828
unknown <gstricat@EN610302.endava.net>
2017-07-04 15:54:46 +0300

    Small changes 9

--- src/main/java/george/stricat/io/justgifit/controller/UploadController.java
+++ src/main/java/george/stricat/io/justgifit/controller/UploadController.java
@@ -38,7 +38,7 @@ import java.lang.invoke.MethodHandles;
             //TODO
         }
 
-        System.out.println("Small changes 8");
+        System.out.println("Small changes 9");
 
         LOG.info("Saved file to {}", videoFile.getAbsolutePath());
         return "";
