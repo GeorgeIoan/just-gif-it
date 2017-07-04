123d21afa35fc99682c374a59f7b7e713d1152b6
George Stricat <george.stricat@gmail.com>
2017-07-04 16:27:08 +0300

    ffsfsgdfgfgdf

--- src/main/java/george/stricat/io/justgifit/controller/UploadController.java
+++ src/main/java/george/stricat/io/justgifit/controller/UploadController.java
@@ -38,7 +38,7 @@ import java.lang.invoke.MethodHandles;
             //TODO
         }
 
-        System.out.println("Small changes 14");
+        System.out.println("Small changes 15");
 
         LOG.info("Saved file to {}", videoFile.getAbsolutePath());
         return "";
