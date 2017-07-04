c5a873e05e83e383aa663a30c352d0980f05342b
George Stricat <George.Stricat@endava.com>
2017-07-04 14:40:32 +0300

    Third commit on UploadController

--- src/main/java/george/stricat/io/justgifit/controller/UploadController.java
+++ src/main/java/george/stricat/io/justgifit/controller/UploadController.java
@@ -38,7 +38,7 @@ import java.lang.invoke.MethodHandles;
             //TODO
         }
 
-        System.out.println("Small changes 2");
+        System.out.println("Small changes 3");
 
         LOG.info("Saved file to {}", videoFile.getAbsolutePath());
         return "";
